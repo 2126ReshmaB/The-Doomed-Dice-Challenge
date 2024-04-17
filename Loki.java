e0263f26d9f404ca53a1388af98ff38ab68475df
Reshma B <118461173+2126ReshmaB@users.noreply.github.com>
2024-04-17 22:58:42 +0530

    Update Loki.java

--- Loki.java
+++ Loki.java
@@ -1,55 +1,100 @@
 import java.util.HashMap;
-import java.util.*;
+import java.util.Map;
 
 public class Loki {
-  public static void main(String args[]){
-    int DieA[] = {1,2,3,4,5,6};
-    int DieB[] = {1,2,3,4,5,6};
 
-    HashMap<Integer,Integer> map = new HashMap<>();
-    for(int i=0;i<6;i++){
-      for(int j=0;j<6;j++){
-        int x = DieA[i] + DieB[j];
-        map.put(x,map.getOrDefault(x,0)+1);
-      }
-    }
-
-    int A[] = new int[6];
-    int B[] = new int[6];
-    for(Map.Entry<Integer,Integer> i : map.entrySet()){
-      
-      int sum = i.getKey();
-      int count = i.getValue();
-
-      for(int j : DieA){
-        if(sum - j >= 1 && sum - j <= 6){
-          A[j-1] += count;
+    public static Map<Integer, Double> calculateProbabilities(int[] die) {
+        Map<Integer, Integer> probabilities = new HashMap<>();
+        for (int i : die) {
+            for (int j : die) {
+                int total = i + j;
+                probabilities.put(total, probabilities.getOrDefault(total, 0) + 1);
+            }
         }
-      }
-
-      for(int j : DieB){
-        if(sum - j >= 1 && sum - j <= 6){
-          B[j-1] += count;
+        int totalOutcomes = die.length * die.length;
+        Map<Integer, Double> result = new HashMap<>();
+        for (Map.Entry<Integer, Integer> entry : probabilities.entrySet()) {
+            result.put(entry.getKey(), entry.getValue() / (double) totalOutcomes);
         }
-      }
+        return result;
     }
 
-    for(int i=0;i<A.length;i++){
-      if(A[i] > 4){
-        int t = A[i] - 4;
-        A[i] -= t;
-        B[i] += t;
-      }
+    public static double[][] transformDice(int[] dieA, int[] dieB) {
+        Map<Integer, Double> probabilitiesA = calculateProbabilities(dieA);
+        Map<Integer, Double> probabilitiesB = calculateProbabilities(dieB);
+        System.out.println(probabilitiesA.get(11) * 2);;
+        double[] newDieA = new double[6];
+        double[] newDieB = new double[6];
+
+        for (int faceB : dieB) {
+            double faceA = 0;
+            for (Map.Entry<Integer, Double> entry : probabilitiesA.entrySet()) {
+                int x = entry.getKey();
+                double probA = entry.getValue();
+                faceA += probA * countOccurrences(newDieA, sum(faceB) - x);
+              
+            }
+            newDieB[faceB - 1] = (int) faceA;
+        }
+
+        for (int i = 0; i < 6; i++) {
+            int numSpots = countOccurrences(newDieB, i + 1);
+            if (numSpots > 4) {
+                int excessSpots = numSpots - 4;
+                for (int j = 0; j < excessSpots; j++) {
+                    removeElement(newDieB, i + 1);
+                    int index = indexOf(newDieA, i + 1);
+                    if (index != -1) {
+                        newDieA[index] = i + 2;
+                    }
+                }
+            }
+        }
+
+        return new double[][]{newDieA, newDieB};
     }
 
-    System.out.println("The DieA become:");
-    for(int i : A){
-      System.out.print(i+" ");
+    public static int countOccurrences(double[] newDieB, int target) {
+        int count = 0;
+        for (double num : newDieB) {
+            if (num == target) {
+                count++;
+            }
+        }
+        return count;
     }
-    System.out.println();
-    System.out.println("The DieB become:");
-    for(int i : B){
-      System.out.print(i+" ");
+
+    public static void removeElement(double[] arr, int target) {
+        int index = indexOf(arr, target);
+        if (index != -1) {
+            for (int i = index; i < arr.length - 1; i++) {
+                arr[i] = arr[i + 1];
+            }
+            arr[arr.length - 1] = 0;
+        }
     }
-  }
+
+    public static int indexOf(double[] arr, int target) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                return i;
+            }
+        }
+        return -1;
+    }
+    public static int sum(int... a){
+        int s = 0;
+        for(int i : a){
+            s += i;
+        }
+        return s;
+    }
+
+    public static void main(String[] args) {
+        int[] dieA = {1, 2, 3, 4, 5, 6};
+        int[] dieB = {1, 2, 3, 4, 5, 6};
+        double[][] newDice = transformDice(dieA, dieB);
+        System.out.println("New Die A: " + java.util.Arrays.toString(newDice[0]));
+        System.out.println("New Die B: " + java.util.Arrays.toString(newDice[1]));
+    }
 }
