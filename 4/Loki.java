0b18c274790e236471354a968f83f85fcea9b1ef
2126ReshmaB <resh21331.cs@rmkec.ac.in>
2024-05-02 17:54:05 +0530

    commited

--- 4/Loki.java
+++ 4/Loki.java
@@ -1,55 +1,54 @@
 import java.util.HashMap;
-import java.util.*;
+import java.util.Map;
 
 public class Loki {
-  public static void main(String args[]){
-    int DieA[] = {1,2,3,4,5,6};
-    int DieB[] = {1,2,3,4,5,6};
 
-    HashMap<Integer,Integer> map = new HashMap<>();
-    for(int i=0;i<6;i++){
-      for(int j=0;j<6;j++){
-        int x = DieA[i] + DieB[j];
-        map.put(x,map.getOrDefault(x,0)+1);
-      }
-    }
+    public static void main(String[] args) {
+        int[] Die_A = {1, 2, 3, 4, 5, 6};
+        int[] Die_B = {1, 2, 3, 4, 5, 6};
 
-    int A[] = new int[6];
-    int B[] = new int[6];
-    for(Map.Entry<Integer,Integer> i : map.entrySet()){
-      
-      int sum = i.getKey();
-      int count = i.getValue();
+        int[] newDieA = undoomDice(Die_A, Die_B);
+        int[] newDieB = Die_B;
 
-      for(int j : DieA){
-        if(sum - j >= 1 && sum - j <= 6){
-          A[j-1] += count;
+        System.out.println("New Die A:");
+        for (int value : newDieA) {
+            System.out.print(value + " ");
         }
-      }
-
-      for(int j : DieB){
-        if(sum - j >= 1 && sum - j <= 6){
-          B[j-1] += count;
+        System.out.println("\nNew Die B:");
+        for (int value : newDieB) {
+            System.out.print(value + " ");
         }
-      }
     }
 
-    for(int i=0;i<A.length;i++){
-      if(A[i] > 4){
-        int t = A[i] - 4;
-        A[i] -= t;
-        B[i] += t;
-      }
+    public static int[] undoomDice(int[] Die_A, int[] Die_B) {
+        Map<Integer, Integer> originalProbabilities = calculateProbabilities(Die_A, Die_B);
+        int[] newDieA = {1, 2, 3, 4, 1, 2}; 
+        Map<Integer, Integer> newProbabilities = calculateProbabilities(newDieA, Die_B);
+
+        double scalingFactor = calculateScalingFactor(originalProbabilities, newProbabilities);
+
+        
+        for (int key : newProbabilities.keySet()) {
+            newProbabilities.put(key, (int) (newProbabilities.get(key) * scalingFactor));
+        }
+
+        return newDieA;
     }
 
-    System.out.println("The DieA become:");
-    for(int i : A){
-      System.out.print(i+" ");
+    public static Map<Integer, Integer> calculateProbabilities(int[] Die_A, int[] Die_B) {
+        Map<Integer, Integer> probabilities = new HashMap<>();
+        for (int a : Die_A) {
+            for (int b : Die_B) {
+                int sum = a + b;
+                probabilities.put(sum, probabilities.getOrDefault(sum, 0) + 1);
+            }
+        }
+        return probabilities;
     }
-    System.out.println();
-    System.out.println("The DieB become:");
-    for(int i : B){
-      System.out.print(i+" ");
+
+    public static double calculateScalingFactor(Map<Integer, Integer> originalProbabilities, Map<Integer, Integer> newProbabilities) {
+        int originalTotal = originalProbabilities.values().stream().mapToInt(Integer::intValue).sum();
+        int newTotal = newProbabilities.values().stream().mapToInt(Integer::intValue).sum();
+        return (double) originalTotal / newTotal;
     }
-  }
 }
